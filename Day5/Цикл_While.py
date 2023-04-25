ea7e0989ca10329d14113fed6cc611b7a1898bdf
FlunkeUA <d.protsenko87@gmail.com>
2023-04-25 15:57:04 +0300

    First commit

---
+++ Day5/Цикл_While.py
@@ -0,0 +1,12 @@
+N = int(input("Enter your number: "))
+
+for N in range(1, N+1, 1):
+    if N % 3 == 0:
+        print(N, end=" ")
+
+summ = 0
+
+for N in range(1, N+1):
+    if N % 3 == 0:
+        summ += N
+print("\n", summ)
