5ad2b35761de243f23538f44fd9661d3c23834d3
FlunkeUA <d.protsenko87@gmail.com>
2023-04-30 22:02:23 +0300

    First commit

--- Day6/Складність_пароля.py
+++ Day6/Складність_пароля.py
@@ -1,1 +1,34 @@
-A = input("Введіть ваш пароль: ")
+password = input("Введіть пароль: ")
+mark = 0
+
+# Перевірка довжини
+if len(password) >= 8:
+    mark = mark + 1
+else:
+    print("Довжина паролю менше 8 символів")
+
+# Перевірка на цифри
+if any(char.isdigit() for char in password):
+    mark = mark + 1
+else:
+    print("Пароль не містить цифр")
+
+# Перевірка на великі літери
+if any(char.isupper() for char in password):
+    mark = mark + 1
+else:
+    print("Пароль не містить великих літер")
+
+# Перевірка на маленькі літери
+if any(char.islower() for char in password):
+    mark = mark + 1
+else:
+    print("Пароль не містить маленьких літер")
+
+# Перевірка на спецсимволи
+if any(not char.isalnum() for char in password):
+    mark = mark + 1
+else:
+    print("Пароль не містить спецсимволів")
+
+print("\nСкладніть паролю =", mark, "/ 5")
