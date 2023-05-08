847dcc05ad04e0b37c9920e5085c91b1fe5c0a99
FlunkeUA <d.protsenko87@gmail.com>
2023-05-08 16:19:41 +0300

    First commit

---
+++ Lesson8/Списки.py
@@ -0,0 +1,26 @@
+print("Завдання 1: ")
+
+number1 = int(input('Введіть число 1: '))
+number2 = int(input('Введіть число 2: '))
+number3 = int(input('Введіть число 3: '))
+number4 = int(input('Введіть число 4: '))
+number5 = int(input('Введіть число 5: '))
+numbers = number1, number2, number3, number4, number5
+
+List = []
+List.extend(numbers)
+
+print(List)
+
+print("Завдання 2: ")
+
+A = [1, 2, 3, 4, 5]
+
+A.pop()
+print(A)
+
+print("Завдання 3: ")
+
+N = int(input('Введіть число :'))
+print(List.count(N))
+
--- Заметки/Test.py
+++ Заметки/Test.py
@@ -15,24 +15,30 @@
 #     sum = sum + i
 # print(sum)
 
-print("Перевірка на поліндром/не поліндром")
-word = input("Введіть текст: ").lower()
-re_word = word[::-1].lower()
-print("палиндром" if re_word == word else "не палиндром")
+# print("Перевірка на поліндром/не поліндром")
+# word = input("Введіть текст: ").lower()
+# re_word = word[::-1].lower()
+# print("палиндром" if re_word == word else "не палиндром")
+#
+# print("\nЗнайти слово в тексті")
+# text = input("Введіть текст: ").lower()
+# word = input("Введіть слово: ").lower()
+# print("yes" if word in text else "no")
+#
+# print("\nЗаміна та додавання тексту")
+# text = input("Введіть текст: ").lower()
+# print(text[:3].replace('abc', 'www')+text[3:] if "abc" in text[:3] else text+"qqq")
+#
+# print("\nВидалення цифр")
+# text = input("Введіть текст: ").lower()
+# print(''.join([x for x in text if not x.isdigit()]))
+#
+# print("\nПеревірка пошти")
+# email = input("Введіть email: ").lower()
+# print("No" if "." not in email or "@" not in email else "Yes")
 
-print("\nЗнайти слово в тексті")
-text = input("Введіть текст: ").lower()
-word = input("Введіть слово: ").lower()
-print("yes" if word in text else "no")
+entered_list = input("Введите список чисел, разделенных пробелом: ").split()
+A = list(map(int, entered_list))
+A.extend('10')
 
-print("\nЗаміна та додавання тексту")
-text = input("Введіть текст: ").lower()
-print(text[:3].replace('abc', 'www')+text[3:] if "abc" in text[:3] else text+"qqq")
-
-print("\nВидалення цифр")
-text = input("Введіть текст: ").lower()
-print(''.join([x for x in text if not x.isdigit()]))
-
-print("\nПеревірка пошти")
-email = input("Введіть email: ").lower()
-print("No" if "." not in email or "@" not in email else "Yes")
+print("Введенный список:", A)
