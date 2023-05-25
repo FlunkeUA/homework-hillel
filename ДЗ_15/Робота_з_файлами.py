11ed0dfc1da2b5943f183dec04f688bd82eae090
FlunkeUA <d.protsenko87@gmail.com>
2023-05-25 14:33:37 +0300

    First commit

--- ДЗ_15/Робота_з_файлами.py
+++ ДЗ_15/Робота_з_файлами.py
@@ -2,90 +2,56 @@ import random
 from collections import Counter
 
 print('Завдання: 1')
-f = open('text.txt')
-text = f.read()
-
-numbers = []
-
-for _ in text:
-    if _.isdigit():
-        numbers.append(_)
-
-print(numbers)
-
-f.close()
+with open('text.txt') as f:
+    text = f.readline()
+    numbers = []
+    for items in text:
+        if items.isdigit():
+            numbers.append(items)
+    print(numbers)
 
 print('\nЗавдання: 2')
 
-text = input("Введіть текст: ")
-f_w = open('data.txt', 'a')
-f_w.write(text)
-f_w.close()
+with open('data.txt', 'a') as file:
+    text = input("Введіть текст: ")
+    file.write(text)
 
 print("\nЗавдання: 3")
-N = int(input('Кількість чисел: '))
-numbers = []
-
-for _ in range(N):
-    i = input('Введіть число: ')
-    numbers.append(i)
-
-numbers = " ".join(numbers)
-
-f_w = open('numbers.txt', 'a')
-f_w.write(numbers)
-f_w.close()
+with open('numbers.txt', 'a') as file:
+    amount_of_numbers = int(input('Кількість чисел: '))
+    for num in range(amount_of_numbers):
+        numbers = input('Введіть число: ')
+        file.write(f"{numbers} ")
 
 print('\nЗавдання: 4')
-n = []
-
-for i in range(100):
-    n.append(random.randint(1, 100))
-
-n = " \n".join(str(x) for x in n)
-
-f_w = open('random_numbers.txt', 'a')
-f_w.write(n)
-f_w.close()
+with open("random_numbers.txt", 'w') as file:
+    for _ in range(100):
+        numbers = random.randint(1, 100)
+        file.write(f"{numbers}\n")
 
 print('\nЗавдання: 5')
-file = open('text.txt')
-words = 0
-
-for line in file:
-    words += len(line.split())
-
-print("Кількість слів:", words)
-
-file.close()
+with open('text.txt') as file:
+    words = 0
+    for line in file:
+        words += len(line.split())
+    print("Кількість слів:", words)
 
 print('\nЗавдання: 6')
-
-file = open('numbers.txt')
-a = file.read()
-numbers = []
-s = 0
-
-for _ in a:
-    if _.isdigit():
-        numbers.append(_)
-
-numbers = [int(x) for x in numbers]
-
-for x in numbers:
-    s += x
-
-print(s)
-file.close()
+with open('numbers.txt', 'r') as file:
+    file_numbers = file.readline()
+    numbers = []
+    summ = 0
+    for item in file_numbers:
+        if item.isdigit():
+            numbers.append(item)
+    for num in numbers:
+        summ += int(num)
+    print(summ)
 
 print('\nЗавдання: 7')
-
-file = open('test.txt')
-a = file.read()
-a = a.split()
-
-c = Counter(a).most_common(5)
-c = ','.join(map(str, c))
-
-print(c)
-file.close()
+with open('test.txt') as file:
+    text = file.read()
+    text = text.split()
+    most_common_count = Counter(text).most_common(5)
+    most_common_count = ','.join(map(str, most_common_count))
+    print(most_common_count)
