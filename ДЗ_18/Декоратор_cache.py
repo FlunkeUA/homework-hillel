a7ed25c041cc4bd10e6d2b2c6771fc37679cd17e
FlunkeUA <d.protsenko87@gmail.com>
2023-06-20 15:13:55 +0300

    First commit

--- ДЗ_18/Декоратор_cache.py
+++ ДЗ_18/Декоратор_cache.py
@@ -1,8 +1,8 @@
 def cahce_dec(f):
     cache = dict()
-    def cache_func(*args):
+    def cache_func(*args, **kwargs):
         if args not in cache:
-            cache[args] = f(*args)
+            cache[args] = f(*args, **kwargs)
         return cache[args]
     return cache_func
 
@@ -24,7 +24,7 @@ def numbers_range(start, end):
             numbers.append(i)
     return numbers
 
-print("Результат выполнения:", sum_range(2, 5))
+print("Результат выполнения:", sum_range(a = 2, b = 5))
 print("Результат выполнения:", sum_range(8, 9))
 print("Результат выполнения:", numbers_range(2, 16))
 print("Результат выполнения:", numbers_range(2, 8))
