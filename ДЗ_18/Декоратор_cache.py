930e6297b7d9c6c97696b39a89cf7ca16e4ec1bb
FlunkeUA <d.protsenko87@gmail.com>
2023-06-21 17:16:17 +0300

    add - func

--- ДЗ_18/Декоратор_cache.py
+++ ДЗ_18/Декоратор_cache.py
@@ -1,9 +1,10 @@
-def cahce_dec(f):
+def cahce_dec(func):
     cache = {}
     def cache_func(*args, **kwargs):
         cache_key = args + tuple(kwargs.items())
         if cache_key not in cache:
-            cache[cache_key] = f(*args, **kwargs)
+            cache[cache_key] = func(*args, **kwargs)
+            print('Память кэша:', cache)
         return cache[cache_key]
     return cache_func
 
@@ -19,14 +20,14 @@ def numbers_range(a, b):
     print(f'Вызвана функция numbers_range с аргументами {a} и {b}')
     numbers = []
     if a > b:
-        start, end = a, b
+        b, a = a, b
     for i in range(a, b + 1):
         if i % 2 == 0:
             numbers.append(i)
     return numbers
 
-print("Результат выполнения 1:", sum_range(a = 2, b = 5))
-print("Результат выполнения 2:", sum_range(b = 8, a = 9))
+print("Результат выполнения 1:", sum_range(a=2, b=5))
+print("Результат выполнения 2:", sum_range(b=8, a=9))
 print("Результат выполнения 3:", numbers_range(2, 16))
 print("Результат выполнения 4:", numbers_range(2, 8))
 print("Результат выполнения 5:", sum_range(2, 5))
