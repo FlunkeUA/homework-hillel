4b4c2054bf57e1a909e4f8304a4b470b580beca5
FlunkeUA <d.protsenko87@gmail.com>
2023-06-26 15:21:36 +0300

    Last commit

--- ДЗ_19_Підсумкова_робота/Крестики_Нолики.py
+++ ДЗ_19_Підсумкова_робота/Крестики_Нолики.py
@@ -1,19 +1,19 @@
 from random import randint
 
-# глобальные константы
-EMPTY = " "
-TIE = "TIE"
-NUM_SQUARES = 9
 
+# запрашиваем куда будет ход
 def ask_number(question, low, high):
-    """Просим ввести число из диапазона"""
     response = None
     while response not in range(low, high):
-        response = int(input(question))
+        try:
+            response = int(input(question))
+        except ValueError:
+            print("Вы ввели не число из диапазона 0-8!!!")
     return response
 
-def pieces():
-    """Определяем принадлежность перового хода."""
+
+# определяем кто будет ходить первый
+def first_move():
     go_first = randint(0, 1)
     if go_first == 1:
         print("\nПервым ходит Человек!")
@@ -25,15 +25,17 @@ def pieces():
         human = 'O'
     return computer, human
 
+
+# создаем игровую доску
 def new_board():
-    """Создаём новую игровую доску."""
     board = []
-    for square in range(NUM_SQUARES):
-        board.append(EMPTY)
+    for square in range(9):
+        board.append(" ")
     return board
 
+
+# Отображаем игровую доску на экране
 def display_board(board):
-    """Отображаем игровую доску на экране."""
     print("\n\t", board[0], "|", board[1], "|", board[2])
     print("\t", "----------")
     print("\t", board[3], "|", board[4], "|", board[5])
@@ -41,16 +43,17 @@ def display_board(board):
     print("\t", board[6], "|", board[7], "|", board[8])
 
 
-def legal_moves(board):
-    """Создаём список доступных ходов."""
+# создаем список доступных ходов
+def available_moves(board):
     moves = []
-    for square in range(NUM_SQUARES):
-        if board[square] == EMPTY:
+    for square in range(9):
+        if board[square] == " ":
             moves.append(square)
     return moves
 
+
+# определяем победителя
 def winner(board):
-    """Определяем победителя в игре."""
     win_combination = (
         (0, 1, 2), (3, 4, 5), (6, 7, 8),  # по горизонтали
         (0, 3, 6), (1, 4, 7), (2, 5, 8),  # по вертикали
@@ -58,68 +61,72 @@ def winner(board):
     )
 
     for row in win_combination:
-        if board[row[0]] == board[row[1]] == board[row[2]] != EMPTY:
-            winner = board[row[0]]
-            return winner
-        if EMPTY not in board:
-            return TIE
+        if board[row[0]] == board[row[1]] == board[row[2]] != " ":
+            win = board[row[0]]
+            return win
+        if " " not in board:
+            return "TIE"
     return None
 
-def human_move(board, human):
-    """Получает ход человека"""
-    legal = legal_moves(board)
+
+# алгоритм хода человека
+def human_move(board):
+    legal = available_moves(board)
     move = None
     while move not in legal:
-        move = ask_number("\nТвой ход. Выбери одно из полей (0 - 8):", 0, NUM_SQUARES)
+        move = ask_number("\nТвой ход. Выбери одно из полей (0 - 8):", 0, 9)
         if move not in legal:
-            print("\nЭто поле уже занято. Выбери другое.\n")
+            print("\nЭто поле уже занято. Выбери другое.")
     print("Вы выбрали поле №:", move)
     return move
 
 
+# алгоритм хода компьютера
 def computer_move(board, computer, human):
-    """Определяем ход за компьютер."""
-    # создадим рабочую копию доски, потому что функция будет менять некотороые элементы в списке
+    # создадим рабочую копию доски,
+    # потому что функция будет менять некотороые элементы в списке
     board = board[:]
     # ходы, от лучшего к худшему
     best_moves = (4, 0, 2, 6, 8, 1, 3, 5, 7)
 
     print("Компьютер выбрал поле №:", end=" ")
     # если сдедующим ходом может победить компьютер, выберем этот ход
-    for move in legal_moves(board):
+    for move in available_moves(board):
         board[move] = computer
         if winner(board) == computer:
             print(move)
             return move
         # выполнив проверку этого хода, отменим его (в локальной копии игровой доски)
-        board[move] = EMPTY
+        board[move] = " "
 
     # если следующим ходом может победить чегловек, блокируем этот ход
-    for move in legal_moves(board):
+    for move in available_moves(board):
         board[move] = human
         if winner(board) == human:
             print(move)
             return move
         # выполнив проверку этого хода, отменим его (в локальной копии игровой доски)
-        board[move] = EMPTY
+        board[move] = " "
 
     # поскольку следующим ходом ни одна из сторон не может победить,
     # выберем лучшее из доступных полей
     for move in best_moves:
-        if move in legal_moves(board):
+        if move in available_moves(board):
             print(move)
             return move
 
+
+# алгоритм перехода хода
 def next_turn(turn):
-    """Осуществляет переход хода."""
     if turn == 'X':
         return 'O'
     else:
         return 'X'
 
+
+# варианты результатов игры
 def congrat_winner(the_winner, computer, human):
-    """Поздравляем победителя игры."""
-    if the_winner != TIE:
+    if the_winner != "TIE":
         print("\nТри", the_winner, "в ряд!")
     else:
         print("Ничья!\n")
@@ -128,25 +135,27 @@ def congrat_winner(the_winner, computer, human):
     elif the_winner == human:
         print("Победа! Выиграл Человек!")
 
+
+# предлагаем сыграть заново
 def replay():
-    """Предлагаем сыграть заново"""
     replay_game = input("\nЖелаете переиграть? (Y или N)").upper()
     for _ in replay_game:
-        if replay_game == "Y":
+        if replay_game == "Y" or 'н':
             main()
         else:
             print("Игра закончена!")
             break
 
 
+# запуск игры
 def main():
-    computer, human = pieces()
+    computer, human = first_move()
     turn = 'X'
     board = new_board()
     display_board(board)
     while not winner(board):
         if turn == human:
-            move = human_move(board, human)
+            move = human_move(board)
             board[move] = human
         else:
             move = computer_move(board, computer, human)
@@ -157,5 +166,5 @@ def main():
     congrat_winner(the_winner, computer, human)
     replay()
 
-# запуск программы
+
 main()
